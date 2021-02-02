73ca7a78bd6f8f45f0297a287b294ce1fc89df32
Andre <andrestafford87@gmail.com>
2021-02-01 20:23:14 -0600

    done

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,16 +1,41 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc{
 
 private static final String spinRate = "200-500";
 private String name;
 private String content;
 //basic instructor
 
-    public CD(String name, String content) {
+    public CD(boolean isBlank, boolean isWrittable, int isCapacity, String name, String content) {
+        super(isBlank, isWrittable, isCapacity);
         this.name = name;
         this.content = content;
     }
+
+    //blank CD constructor
+    public CD(boolean isBlank, boolean isRewritable, int capacity) {
+        super(isBlank, isRewritable, capacity);
+    }
+
+        //Music CD constructor
+    public CD(String name, String content) {
+    Super();
+    this.name = name;
+    this.content = content;
+    }
+
+
+    //New CD-RW constructor
+    public CD CDRW(int capacity){
+        return new CD(true,false, capacity);
+    }
+
+    public CD CDR(int capacity){
+        return new CD(true, false, capacity);
+    }
+
+
 
 // TODO: Implement your custom interface.
 
